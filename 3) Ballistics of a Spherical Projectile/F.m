98a6665cde301302e66b0aac5618531b85a2f1ae
WexilerGebe <dmitriy.babinkin@gmail.com>
2021-11-03 16:30:25 +0300

    Right Part Function

---
+++ 3) Ballistics of a Spherical Projectile/F.m
@@ -0,0 +1,20 @@
+function F = F(z, ~)
+
+global rho_ratio d g Uf Vf;
+
+Uf = 0;
+Vf = 0;
+
+U = z(3);
+V = z(4);
+
+w_r = sqrt((Uf - U) ^ 2 + (Vf - V) ^ 2);
+
+
+    
+  
+    Fx = 3 * rho_ratio / (4 * d) * c_d(w_r) * (Uf - z(3)) * w_r / (1 + 0.5 * rho_ratio);
+    Fy = (- (1 - rho_ratio) * g + 3 * rho_ratio / (4 * d) * c_d(w_r) * (Vf - z(4)) * w_r) / (1 + 0.5 * rho_ratio);
+    F = [U;V;Fx;Fy];
+end
+
