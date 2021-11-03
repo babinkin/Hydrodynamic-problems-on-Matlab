d277b9d25caa1189205e0066b2d2787599db35ea
WexilerGebe <dmitriy.babinkin@gmail.com>
2021-11-03 16:28:12 +0300

    Main Function
    Fixed plots

--- 3) Ballistics of a Spherical Projectile/main.m
+++ 3) Ballistics of a Spherical Projectile/main.m
@@ -23,19 +23,31 @@ w0_v = w0 * sin(tetta);
 Uf = 0;
 Vf = 0;
 
-[t1, z1] = RungeKutta(@Fx, @Fy, [t0 tmax], [0 0 w0_u w0_v]);
-[t2, z2] = ode45(@ode_func, [t0 tmax], [0 0 w0_u w0_v ]);
+[t1, z1] = RungeKutta(@F, [t0 tmax], [0 0 w0_u w0_v]);
+%[t2, z2] = ode45(@F, [t0 tmax], [0 0 w0_u w0_v ]);
 
-x1 = z1(:, 1);
-y1 = z1(:, 2);
-x2 = z2(:, 1);
-y2 = z2(:, 2);
+x = z1(:, 1);
+y = z1(:, 2);
+U =  z1(:, 3);
+V =  z1(:, 4);
+
+%x2 = z2(:, 1);
+%y2 = z2(:, 2);
+
+% figure(1)
+%  plot(t1, z1, 'r --'  , t2, z2, 'b-' );
+% title('y(x)');
+% xlabel('x');
+% ylabel('y');
+% xlim([0 300]);
+% ylim([0 100]);
+% legend('RungeKutta2d', 'ode45', 'Location', 'best');
 
 figure(1)
-plot(t1, z1, 'r --', t2, z2, 'b-');
+ plot(x, y, 'r --');
 title('y(x)');
 xlabel('x');
 ylabel('y');
-xlim([0 300]);
+xlim([0 200]);
 ylim([0 100]);
-legend('RungeKutta2d', 'ode45', 'Location', 'best');
+legend('RungeKutta2d', 'Location', 'best');
