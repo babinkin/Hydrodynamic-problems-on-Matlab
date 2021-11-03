dff94ce08fe9683053277e33b0427618dc32ff19
WexilerGebe <dmitriy.babinkin@gmail.com>
2021-11-03 17:03:22 +0300

    Main Function
    Added new angles tetta

--- 3) Ballistics of a Spherical Projectile/main.m
+++ 3) Ballistics of a Spherical Projectile/main.m
@@ -1,9 +1,9 @@
 clc;
 clear;
 
-global rho_ratio d nu g h Uf Vf w0 tetta;
+global rho_ratio d nu g h Uf Vf w0;
 
-rho_f = 1.22 ; %воздух
+rho_f = 1.22 ; %1.22 для воздуха, 0 для вакуума  
 rho = 8000; % сталь
 rho_ratio = rho_f / rho;
 
@@ -14,40 +14,54 @@ g = 9.8;
 t0 = 0;
 tmax = 100; % до момента падения снаряда на землю
 
-tetta = 30 * pi / 180; %45, 60
+tetta30 = 30 * pi / 180; % угол между начальной скоростью и OX
 w0 = 50; %начальная скорость
 
-w0_u = w0 * cos(tetta);
-w0_v = w0 * sin(tetta);
+w0_u30 = w0 * cos(tetta30);
+w0_v30 = w0 * sin(tetta30);
 
 Uf = 0;
 Vf = 0;
 
-[t1, z1] = RungeKutta(@F, [t0 tmax], [0 0 w0_u w0_v]);
-%[t2, z2] = ode45(@F, [t0 tmax], [0 0 w0_u w0_v ]);
+%Расчёт для тетта = 30
+[t1, z1] = RungeKutta(@F, [t0 tmax], [0 0 w0_u30 w0_v30]);
+x1 = z1(:, 1);
+y1 = z1(:, 2);
+U1 =  z1(:, 3);
+V1 =  z1(:, 4);
 
-x = z1(:, 1);
-y = z1(:, 2);
-U =  z1(:, 3);
-V =  z1(:, 4);
 
-%x2 = z2(:, 1);
-%y2 = z2(:, 2);
 
-% figure(1)
-%  plot(t1, z1, 'r --'  , t2, z2, 'b-' );
-% title('y(x)');
-% xlabel('x');
-% ylabel('y');
-% xlim([0 300]);
-% ylim([0 100]);
-% legend('RungeKutta2d', 'ode45', 'Location', 'best');
+%Расчёт для тетта = 45
+tetta45 = 45 * pi / 180; 
+w0_u45 = w0 * cos(tetta45);
+w0_v45 = w0 * sin(tetta45);
+
+[t2, z2] = RungeKutta(@F, [t0 tmax], [0 0 w0_u45 w0_v45]);
+x2 = z2(:, 1);
+y2 = z2(:, 2);
+U2 =  z2(:, 3);
+V2 =  z2(:, 4);
+
+
+%Расчёт для тетта = 60
+tetta60 = 60 * pi / 180; 
+w0_u60 = w0 * cos(tetta60);
+w0_v60 = w0 * sin(tetta60);
+
+[t3, z3] = RungeKutta(@F, [t0 tmax], [0 0 w0_u60 w0_v60]);
+x3 = z3(:, 1);
+y3 = z3(:, 2);
+U3 =  z3(:, 3);
+V3 =  z3(:, 4);
+
+
 
 figure(1)
- plot(x, y, 'r --');
+ plot(x1, y1, 'r -',x2, y2, 'b -',x3, y3, 'g -');
 title('y(x)');
 xlabel('x');
 ylabel('y');
-xlim([0 200]);
+xlim([0 300]);
 ylim([0 100]);
-legend('RungeKutta2d', 'Location', 'best');
+legend('tetta = 30','tetta = 45','tetta = 60', 'Location', 'best');
