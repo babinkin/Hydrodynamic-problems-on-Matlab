6787202fd8377e82b171920b710a99145446d35b
WexilerGebe <dmitriy.babinkin@gmail.com>
2021-11-06 13:52:19 +0300

    Flight Range Search Function

---
+++ 3) Ballistics of a Spherical Projectile/Find_Range.m
@@ -0,0 +1,22 @@
+function [max_range] = Find_Range(t0, x0, y0, u0, v0) %d, w0, epsilon  % tetta_optimal
+
+global h;
+
+[t, x, y, u, v] = flightPath(t0, x0, y0, u0, v0);
+
+
+x_q = x(end);
+y_q = y(end);
+
+
+[~, Y] = ode45(@F, [t(end), t(end) -  h], [x(end), y(end), u(end), v(end)]);
+
+x_p = Y(end, 1);
+y_p = Y(end, 2);
+
+% дальность полёта
+max_range = (x_q * y_p - x_p * y_q) /(y_p - y_q);
+
+disp(max_range);
+end
+
