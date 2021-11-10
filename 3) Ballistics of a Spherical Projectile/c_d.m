a3391219fbda2df89a584770af06402b7520d6e8
WexilerGebe <dmitriy.babinkin@gmail.com>
2021-11-10 17:23:29 +0300

    Lift Coefficient

--- 3) Ballistics of a Spherical Projectile/c_d.m
+++ 3) Ballistics of a Spherical Projectile/c_d.m
@@ -4,22 +4,22 @@ global d nu ;
 
 Re = w_r * d / nu;
 
-%liftCoefficient = 0.4; %Problem 1.8
+liftCoefficient = 0.4; %Problem 1.8
 
- if Re == 0
-        c_d = 0;
-    elseif Re <= 1
-        c_d = 24 / Re;
-    elseif (1 < Re) && (Re <= 400)
-        c_d = 24 / Re^(0.646);
-    elseif (400 < Re) && (Re <= 3e5)
-        c_d = 0.5;
-    elseif (3e5 < Re) && (Re <= 2e6)
-        c_d = 0.000366 * Re^(0.4275);
-    elseif Re > 2e6
-        c_d = 0.18;
- end
-    liftCoefficient = c_d;
+%  if Re == 0
+%         c_d = 0;
+%     elseif Re <= 1
+%         c_d = 24 / Re;
+%     elseif (1 < Re) && (Re <= 400)
+%         c_d = 24 / Re^(0.646);
+%     elseif (400 < Re) && (Re <= 3e5)
+%         c_d = 0.5;
+%     elseif (3e5 < Re) && (Re <= 2e6)
+%         c_d = 0.000366 * Re^(0.4275);
+%     elseif Re > 2e6
+%         c_d = 0.18;
+%  end
+%     liftCoefficient = c_d;
 
 end
 
