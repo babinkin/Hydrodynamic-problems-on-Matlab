9b32fd4108a70ea49926015b88935d7fcd61f6f1
WexilerGebe <dmitriy.babinkin@gmail.com>
2021-11-16 14:02:57 +0300

    Main Function
    For Glider

--- 4) Flight Part of Glider/main.m
+++ 4) Flight Part of Glider/main.m
@@ -1,12 +1,52 @@
 clc;
 clear;
 
-global m_p_0;
+global A B h; 
 
-g = 9.8;                  % gravitational acceleration
-rho = 1.23;             % air density
-A = 0.1;                 % max cross-sectional area
-m_c = 500 / g;        % mass of rocket casing
-V_e = 360;             % exhaust speed
-C_d = 0.15;            % drag coefficient
-m_p_0 = 1000 / g;   % initial mass of the propellant at t = 0
+h = 0.1; %time step
+tm = 10; %end computation time
+
+
+%Non-dimension coefficients A and B
+%A = c_l * rho * w0 ^ 2 * S / 2 / m / g;
+A = 1.5; 
+
+%B = c_d / c_l;
+B = 0.06;
+
+W0 = 1; %initial velocity
+tetta_vertical =  -90 * pi / 180; %angle between W and OX for verical motion
+
+U0_verical = W0 * cos(tetta_vertical); 
+V0_vertical = W0 * sin(tetta_vertical);
+
+tetta_upsideDown = 180 * pi / 180; %angle between W and OX for upside down motion
+
+U0_upsideDown= W0 * cos(tetta_upsideDown);
+V0_upsideDown = W0 * sin(tetta_upsideDown);
+
+
+ [t1, y1] = RungeKutta(@F, [0, tm], [0, 0, U0_verical, V0_vertical]);
+ [t2, y2] = RungeKutta(@F, [0, tm], [0, 0, U0_upsideDown, V0_upsideDown]);
+
+X_vertical = y1( :,1);
+Y_vertical = y1(:, 2);
+U_vertical = y1(:, 3);
+V_vertical = y1(:, 4);
+
+X_upsideDown = y2( :,1);
+Y_upsideDown = y2(:, 2);
+U_upsideDown = y2(:, 3);
+V_upsideDown = y2(:, 4);
+
+%Вывод траектории полёта
+figure(1)
+ plot(X_vertical, Y_vertical, 'r -',X_upsideDown, Y_upsideDown, 'b --');
+title('y(x)');
+xlabel('x');
+ylabel('y');
+xlim([-1 5]);
+ylim([-2 1]);
+legend('Vertical flight','Upside down flight', 'Location', 'best');
+
+
