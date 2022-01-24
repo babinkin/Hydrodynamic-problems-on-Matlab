6075ea70343076c70c04ab02c21f62fb9581f21a
WexilerGebe <dmitriy.babinkin@gmail.com>
2022-01-24 14:27:53 +0300

    Successive Overrelaxation

---
+++ 8) Solving Elliptic Partial Differential Equations/successiveOverrelaxation.m
@@ -0,0 +1,61 @@
+function [psi_new] = successiveOverrelaxation(ksi)
+
+    global m n h psi_0 error_max;
+    
+    iter = 1;
+    error = 1;
+    
+    psi_new = zeros(m, n);
+    psi_old = zeros(m, n);
+    
+
+    for i = 1 : m
+        
+        psi_new(i,1) = 0;
+        psi_new(i,n) = 0;
+        
+    end
+
+    for j = 2 : n - 1
+        
+        psi_new(1, j) = 0;
+        psi_new(m, j) = 0;
+        
+    end
+    
+    
+    for i = 2 : m - 1
+        
+        for j = 2 : n - 1
+            
+            psi_new(i, j) = psi_0;
+            
+        end
+        
+    end
+    
+    alpha = cos(pi / m) + cos(pi / n);
+    omega = (8 - 4 * sqrt(4 - alpha ^ 2)) / alpha ^ 2;
+            
+    while error > error_max
+        
+        iter = iter + 1;
+        error = 0;
+                
+                for i = 2 : m - 1
+                    
+                    for j = 2 : n - 1
+                        
+                        psi_new(i, j) = psi_old(i, j) + omega * (psi_new(i - 1, j) + psi_old(i + 1, j) +  psi_new(i, j - 1) + psi_old(i, j + 1) -  4 * psi_old(i, j) + h ^ 2 * ksi(i, j)) / 4;
+                        error = error + abs(psi_new(i, j) - psi_old(i, j));
+                        psi_old(i, j) = psi_new(i, j);
+                        
+                    end
+                    
+                end
+                
+                iter = iter + 1;
+                
+    end
+end
+  
